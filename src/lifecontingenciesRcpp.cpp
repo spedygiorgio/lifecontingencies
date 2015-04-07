b8d9b4a2fd5a297b3e49bda2523956176d2c815f
Giorgio Alfredo Spedicato <spedygiorgio@gmail.com>
2015-04-07 23:18:38 +0200

    some more functs

--- src/lifecontingenciesRcpp.cpp
+++ src/lifecontingenciesRcpp.cpp
@@ -1,4 +1,5 @@
 #include <Rcpp.h>
+#include <math.h> 
 using namespace Rcpp;
 
 
@@ -27,3 +28,57 @@ double mult2sum(NumericVector x, NumericVector y)
   }
   return total;
 }
+
+
+//pure endowment function
+//.fExn<-function(T,y,n, i)
+//{
+//  out=ifelse(T<y+n,0,(1+i)^(-n))
+//	return(out)
+//}
+
+
+// [[Rcpp::export(.fExnCpp)]]
+double fExn(double T, double y, double n, double i)
+{
+  double out;
+  if(T<y+n)
+    out=0;
+  else
+    out=pow(1+i,-n);
+  return out;
+}
+
+
+//.fAxn<-function(T,y,n, i, m, k)
+//{
+//  out=numeric(1)
+//	out=ifelse(((T>=y+m) && (T<=y+m+n-1/k)),(1+i)^-(T-y+1/k),0)
+//	return(out)
+//}
+
+
+// [[Rcpp::export(.fAxnCpp)]]
+double fExn(double T, double y, double n, double i, double m, double k)
+{
+  double out;
+  if ((T>=y+m) && (T<=y+m+n-1/k))
+    out=pow(1+i,-(T-y+1/k));
+  else
+    out=0;
+  return out;
+}
+
+//.fAxyzn<-function(T,y,n, i, m, k, status)
+//{
+//  out=numeric(1)
+//	temp=T-y
+//	T=ifelse(status=="joint",min(temp),max(temp)) #redefines T
+//	out=ifelse(((T>=m) && (T<=m+n-1/k)),(1+i)^-(T+1/k),0)
+//	return(out)
+//}
+
+
+// [[Rcpp::export(.fAxyznCpp)]]
+
+
