5a17961fa41b2bf1a223539cd59bfaba302529f8
Giorgio Alfredo Spedicato <spedygiorgio@gmail.com>
2017-02-20 17:23:03 +0100

    fix

--- src/registerDynamicSymbols.c
+++ src/registerDynamicSymbols.c
@@ -4,7 +4,7 @@
 #include <Rinternals.h>
 #include <R_ext/Rdynload.h>
 
-void R_init_markovchain(DllInfo* info) {
+void R_init_lifecontincencies(DllInfo* info) {
   R_registerRoutines(info, NULL, NULL, NULL, NULL);
   R_useDynamicSymbols(info, TRUE);
 }
