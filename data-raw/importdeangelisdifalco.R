f97902de99a25d7455da26bd0053fdc91038ca61
spedygiorgio <spedygiorgio@gmail.com>
2017-07-09 14:34:13 +0200

    small cleanings and changes

--- data-raw/importdeangelisdifalco.R
+++ data-raw/importdeangelisdifalco.R
@@ -1,3 +1,4 @@
+load("data-raw/deangelisdifalco.RData")
 names(pxHealthyF)[1]<-"age"
 names(pxHealthyM)[1]<-"age"
 names(DisabledFemaleLifeTable)[1]<-"age"
@@ -12,4 +13,16 @@ names(TransitionHealthtoDisM)[1]<-"age"
 de_angelis_di_falco<-list(pxHealthyF,pxHealthyM,DisabledFemaleLifeTable,DisabledMaleLifeTable,qxDisabledF,qxDisabledM,
                           qxHealthyF,qxHealthyM,TransitionHealthtoDisF,TransitionHealthtoDisM)
 
-devtools::use_data(de_angelis_di_falco)
+
+de_angelis_di_falco[["pxHealthyF"]]<-pxHealthyF
+de_angelis_di_falco[["pxHealthyM"]]<-pxHealthyM
+de_angelis_di_falco[["DisabledFemaleLifeTable"]]<-DisabledFemaleLifeTable
+de_angelis_di_falco[["DisabledMaleLifeTable"]]<-DisabledMaleLifeTable
+de_angelis_di_falco[["qxDisabledF"]]<-qxDisabledF
+de_angelis_di_falco[["qxDisabledM"]]<-qxDisabledM
+de_angelis_di_falco[["qxHealthyF"]]<-qxHealthyF
+de_angelis_di_falco[["qxHealthyM"]]<-qxHealthyM
+de_angelis_di_falco[["TransitionHealthtoDisF"]]<-TransitionHealthtoDisF
+de_angelis_di_falco[["TransitionHealthtoDisM"]]<-TransitionHealthtoDisM
+
+devtools::use_data(de_angelis_di_falco, overwrite = TRUE)
