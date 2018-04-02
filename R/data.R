9c6efd84ec1711c3adc6e1d179d5e13f171dfad0
spedygiorgio <spedygiorgio@gmail.com>
2018-04-02 19:49:18 +0200

    recreating old data

--- R/data.R
+++ R/data.R
@@ -5,5 +5,34 @@
 #' and transitions from Healty to Disabled
 #' 
 #' @format a list containing elevent items (data.frames), and an mdt data object (HealthyMaleTable2013)
-#' @source PAOLO DE ANGELIS, LUIGI DI FALCO (a cura di). Assicurazioni sulla salute: caratteristiche, modelli attuariali e basi tecniche
+#' @source PAOLO DE ANGELIS, LUIGI DI FALCO (a cura di). Assicurazioni sulla salute: caratteristiche, 
+#' modelli attuariali e basi tecniche
 "de_angelis_di_falco"
+
+#' @title United States Social Security life tables
+#' 
+#' @description This data set contains period life tables for years 1990, 2000 and 2007. Both males and 
+#' females life tables are reported.
+#' @format A \code{data.frame} containing people surviving at the beginning of "age" at 2007, 2000, and 1990
+#' split by gender
+#' @details Reported age is truncated at the last age with lx>0.
+#' @source See \url{http://www.ssa.gov/oact/NOTES/as120/LifeTables_Body.html}
+#' @examples
+#' data(demoUsa)
+#' head(demoUsa)
+"demoUsa"
+
+#' @title Society of Actuaries life table
+#' @description This table has been used by the classical book Actuarial Mathematics and by the Society 
+#' of Actuaries for US professional examinations.
+#' @usage data(soaLt)
+#' @format A \code{data.frame} with 111 obs on the following 2 variables:
+#' \describe{ \item{\code{x}}{a numeric vector} \item{\code{Ix}}{a numeric vector} }
+#' @details Early ages have been found elsewere since miss in the original data sources; SOA did not 
+#' provide population at risk data for certain spans of age (e.g. 1-5, 6-9, 11-14 and 16-19)
+#' @references Actuarial Mathematics (Second Edition), 1997, by Bowers, N.L., Gerber, H.U., 
+#' Hickman, J.C., Jones, D.A. and Nesbitt, C.J.
+#' @examples 
+#' data(soaLt)
+#' head(soaLt)
+"soaLt"
---
+++ data-raw/create_old_data_file.R
@@ -0,0 +1,16 @@
+require(data.table)
+#demoUsa
+demoUsa<-fread(input = "./data-raw/demoUsa.csv",data.table = FALSE,encoding = "UTF-8")
+devtools::use_data(demoUsa,overwrite = TRUE)
+#soaLt
+soaLt<-fread(input = "./data-raw/soaLt.csv",data.table = FALSE,encoding = "UTF-8")
+devtools::use_data(soaLt,overwrite = TRUE)
+#AF92Lt
+#fwrite(x=AF92Lt.data, file = "./data-raw/AF92.csv")
+AF92Lt.data<-as(AF92Lt, "data.frame")
+AF92Lt.data$px<-NULL
+AF92Lt.data$ex<-NULL
+fwrite(x=AF92Lt.data, file = "./data-raw/AF92.csv")
+AF92.data<-fread(input = "./data-raw/AF92.csv",data.table = FALSE, encoding = "UTF-8")
+AF92Lt<-new("lifetable",x=AF92.data$x,lx=AF92.data$lx,name='AF92')
+devtools::use_data(AF92Lt,overwrite = TRUE)
