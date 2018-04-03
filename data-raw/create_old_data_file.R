9f426a46ef9dba6e08502b24b2ea8c53a5951896
Giorgio Alfredo <giorgioalfredo.spedicato@leitha.eu>
2018-04-03 14:27:05 +0200

    add demoUk

--- data-raw/create_old_data_file.R
+++ data-raw/create_old_data_file.R
@@ -1,4 +1,5 @@
 require(data.table)
+require(tidyverse)
 #demoUsa
 demoUsa<-fread(input = "./data-raw/demoUsa.csv",data.table = FALSE,encoding = "UTF-8")
 devtools::use_data(demoUsa,overwrite = TRUE)
@@ -14,3 +15,8 @@ fwrite(x=AF92Lt.data, file = "./data-raw/AF92.csv")
 AF92.data<-fread(input = "./data-raw/AF92.csv",data.table = FALSE, encoding = "UTF-8")
 AF92Lt<-new("lifetable",x=AF92.data$x,lx=AF92.data$lx,name='AF92')
 devtools::use_data(AF92Lt,overwrite = TRUE)
+#demoUk
+data("demoUk")
+fwrite(x=demoUk, file="./data-raw/demoUk.csv")
+demoUk<-fread(input = "./data-raw/demoUk.csv",data.table = FALSE, encoding = "UTF-8")
+devtools::use_data(demoUk,overwrite = TRUE)
