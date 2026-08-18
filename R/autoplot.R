c673bc6bf708deb3f1d11142933cbfee3aa7af4c
Giorgio Alfredo Spedicato <spedygiorgio@users.noreply.github.com>
2026-08-18 10:52:15 +0200

    Fix autoplot to use ggplot2 S3 dispatch

--- R/autoplot.R
+++ R/autoplot.R
@@ -1,8 +1,8 @@
 #' Autoplot a life table
 #'
 #' Produces a ggplot2 representation of the number of survivors by age.
-#' The method is defined for `lifetable` and is therefore also available
-#' for subclasses such as `actuarialtable` through S4 inheritance.
+#' The method is defined for `lifetable`; S3 dispatch also makes it
+#' available for S4 subclasses such as `actuarialtable`.
 #'
 #' @param object A `lifetable` object, or an object inheriting from it.
 #' @param ... Additional arguments passed to `ggplot2::geom_line()`.
@@ -10,18 +10,24 @@
 #' @return A `ggplot2` plot object.
 #' @importFrom ggplot2 autoplot
 #' @export
-setMethod(
-  "autoplot",
-  signature(object = "lifetable"),
-  definition = function(object, ...) {
-    data <- data.frame(x = object@x, lx = object@lx)
+#' @name autoplot.lifetable
+#' @rdname autoplot.lifetable
+#' @aliases autoplot.lifetable
+#' @method autoplot lifetable
+#' @examples
+#' lt <- new("lifetable", x = 0:3, lx = c(100, 90, 50, 10))
+#' autoplot(lt)
+autoplot.lifetable <- function(object, ...) {
+  data <- data.frame(x = object@x, lx = object@lx)
 
-    ggplot2::ggplot(data, ggplot2::aes(x = x, y = lx)) +
-      ggplot2::geom_line(...) +
-      ggplot2::labs(
-        title = paste("Life table", object@name),
-        x = "Age",
-        y = "Number of survivors"
-      )
-  }
-)
+  ggplot2::ggplot(
+    data,
+    ggplot2::aes(x = data[["x"]], y = data[["lx"]])
+  ) +
+    ggplot2::geom_line(...) +
+    ggplot2::labs(
+      title = paste("Life table", object@name),
+      x = "Age",
+      y = "Number of survivors"
+    )
+}
